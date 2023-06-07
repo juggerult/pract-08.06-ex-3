cfe787c8dfb899ab307e3e0e311848d81eb1a5d6
vanko <vanko@LAPTOP-KF2PPAU5>
2023-06-07 18:58:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Zad3_04._06._23
+{
+    partial class frmSector
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtColvoEl = new System.Windows.Forms.TextBox();
+            this.btGenerate = new System.Windows.Forms.Button();
+            this.dgvMass = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chartSector = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btCreateDiag = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMass)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSector)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvMass);
+            this.groupBox1.Controls.Add(this.btGenerate);
+            this.groupBox1.Controls.Add(this.txtColvoEl);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(968, 155);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Входящие данные";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите количество элементов:";
+            // 
+            // txtColvoEl
+            // 
+            this.txtColvoEl.Location = new System.Drawing.Point(11, 50);
+            this.txtColvoEl.Name = "txtColvoEl";
+            this.txtColvoEl.Size = new System.Drawing.Size(257, 26);
+            this.txtColvoEl.TabIndex = 1;
+            // 
+            // btGenerate
+            // 
+            this.btGenerate.Location = new System.Drawing.Point(11, 83);
+            this.btGenerate.Name = "btGenerate";
+            this.btGenerate.Size = new System.Drawing.Size(257, 66);
+            this.btGenerate.TabIndex = 2;
+            this.btGenerate.Text = "Сгенерировать пустой одномерный массив";
+            this.btGenerate.UseVisualStyleBackColor = true;
+            this.btGenerate.Click += new System.EventHandler(this.btGenerate_Click);
+            // 
+            // dgvMass
+            // 
+            this.dgvMass.AllowUserToOrderColumns = true;
+            this.dgvMass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMass.Location = new System.Drawing.Point(287, 25);
+            this.dgvMass.Name = "dgvMass";
+            this.dgvMass.Size = new System.Drawing.Size(675, 124);
+            this.dgvMass.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btExit);
+            this.groupBox2.Controls.Add(this.btCreateDiag);
+            this.groupBox2.Controls.Add(this.chartSector);
+            this.groupBox2.Location = new System.Drawing.Point(13, 175);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(968, 366);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Построение диаграммы";
+            // 
+            // chartSector
+            // 
+            this.chartSector.BackColor = System.Drawing.Color.SkyBlue;
+            this.chartSector.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            this.chartSector.BackSecondaryColor = System.Drawing.Color.Blue;
+            this.chartSector.BorderlineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea3.Name = "ChartArea1";
+            this.chartSector.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartSector.Legends.Add(legend3);
+            this.chartSector.Location = new System.Drawing.Point(11, 26);
+            this.chartSector.Name = "chartSector";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.Legend = "Legend1";
+            series3.Name = "Sector";
+            this.chartSector.Series.Add(series3);
+            this.chartSector.Size = new System.Drawing.Size(808, 340);
+            this.chartSector.TabIndex = 0;
+            this.chartSector.Text = "chart1";
+            // 
+            // btCreateDiag
+            // 
+            this.btCreateDiag.Location = new System.Drawing.Point(840, 26);
+            this.btCreateDiag.Name = "btCreateDiag";
+            this.btCreateDiag.Size = new System.Drawing.Size(122, 103);
+            this.btCreateDiag.TabIndex = 1;
+            this.btCreateDiag.Text = "Построить секторную диаграмму";
+            this.btCreateDiag.UseVisualStyleBackColor = true;
+            this.btCreateDiag.Click += new System.EventHandler(this.btCreateDiag_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(840, 257);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(122, 103);
+            this.btExit.TabIndex = 2;
+            this.btExit.Text = "Выход";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // frmSector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 553);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmSector";
+            this.Text = "Секторная диаграмма";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMass)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartSector)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvMass;
+        private System.Windows.Forms.Button btGenerate;
+        private System.Windows.Forms.TextBox txtColvoEl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btCreateDiag;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSector;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zad3_04._06._23
+{
+    public partial class frmSector : Form
+    {
+        int m;
+        double Sum;
+        public frmSector()
+        {
+            InitializeComponent();
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit ();
+        }
+
+        private void btGenerate_Click(object sender, EventArgs e)
+        {
+            if (txtColvoEl.Text == "")
+            { MessageBox.Show("Количество категорий расходов не введено!", "Ошибка"); }
+            else
+            {
+                m = Int32.Parse(txtColvoEl.Text);
+                if (m > 10)
+                { MessageBox.Show("Количество категорий расходов превышает число 10!", "Ошибка"); }
+                else
+                {
+                    dgvMass.ColumnCount = m;
+
+                    for (int j = 0; j < m; j++)
+                    {
+                        dgvMass.Columns[j].Width = 110;
+                        dgvMass.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                        dgvMass.Columns[j].HeaderText = "Расход категории " + (j + 1).ToString();
+                    }
+                }
+            }
+
+        }
+
+        private void btCreateDiag_Click(object sender, EventArgs e)
+        {
+            Sum = 0;
+            double[] A = new double[m];
+            string[] B = new string[m];
+
+            for (int j = 0; j < m; j++)
+            {
+                A[j] = Convert.ToDouble(dgvMass.Rows[0].Cells[j].Value);
+                B[j] = "Расходы категории " + (j + 1).ToString();
+                Sum += A[j];
+                if (A[j] <= 0)
+                { MessageBox.Show("Среди введённых элементов есть отрицательные или нулевые!", "Ошибка"); }
+            }
+
+            if (Sum != 100)
+            { MessageBox.Show("Сумма процентного содержания расходов должна равняться 100%! Перепроверьте данные!", "Ошибка"); }
+            else
+            {
+                chartSector.Series[0].Points.DataBindXY(B, A);
+                chartSector.ChartAreas[0].Area3DStyle.Enable3D = true;
+            }
+
+        }
+    }
+}
